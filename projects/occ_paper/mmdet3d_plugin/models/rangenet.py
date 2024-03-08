d73909c14ea8f09235861f0bf0a48c01ea16298c
liumu4090 <1356981019@qq.com>
2024-03-08 17:01:35 +0800

    pretrain

--- projects/occ_paper/mmdet3d_plugin/models/rangenet.py
+++ projects/occ_paper/mmdet3d_plugin/models/rangenet.py
@@ -1,15 +1,70 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+# Copyright (c) OpenMMLab. All rights reserved.
+from typing import Optional, Sequence, Tuple
 
-from typing import Sequence, Tuple
-from torch import Tensor
-from mmdet3d.registry import MODELS
-from mmengine.model import BaseModule
+import torch
 from mmcv.cnn import ConvModule, build_activation_layer, build_conv_layer, build_norm_layer
-from mmdet3d.utils import ConfigType, OptConfigType
 from mmengine.model import BaseModule
-from .utils import make_res_layer
+from torch import Tensor, nn
+from torch.nn import functional as F
+
+from mmdet3d.registry import MODELS
+from mmdet3d.utils import ConfigType, OptConfigType, OptMultiConfig
+
+
+class BasicBlock(BaseModule):
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        dilation: int = 1,
+        downsample: Optional[nn.Module] = None,
+        conv_cfg: OptConfigType = None,
+        norm_cfg: ConfigType = dict(type="BN"),
+        act_cfg: ConfigType = dict(type="LeakyReLU"),
+        init_cfg: OptMultiConfig = None,
+    ) -> None:
+        super(BasicBlock, self).__init__(init_cfg)
+
+        self.norm1_name, norm1 = build_norm_layer(norm_cfg, planes, postfix=1)
+        self.norm2_name, norm2 = build_norm_layer(norm_cfg, planes, postfix=2)
+
+        self.conv1 = build_conv_layer(conv_cfg, inplanes, planes, 3, stride=stride, padding=dilation, dilation=dilation, bias=False)
+        self.add_module(self.norm1_name, norm1)
+        self.conv2 = build_conv_layer(conv_cfg, planes, planes, 3, padding=1, bias=False)
+        self.add_module(self.norm2_name, norm2)
+        self.relu = build_activation_layer(act_cfg)
+        self.downsample = downsample
+
+    @property
+    def norm1(self) -> nn.Module:
+        """nn.Module: normalization layer after the first convolution layer."""
+        return getattr(self, self.norm1_name)
+
+    @property
+    def norm2(self) -> nn.Module:
+        """nn.Module: normalization layer after the second convolution layer."""
+        return getattr(self, self.norm2_name)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        ResBlock: two conv layers with a residual connection.
+        """
+        identity = x
+
+        out = self.conv1(x)
+        out = self.norm1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.norm2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        return out
 
 
 @MODELS.register_module()
@@ -45,7 +100,7 @@ class RangeNet(BaseModule):
             stride = strides[i]
             dilation = dilations[i]
             planes = out_channels[i]
-            res_layer = make_res_layer(
+            res_layer = self.make_res_layer(
                 inplanes=inplanes,
                 planes=planes,
                 num_blocks=num_blocks,
@@ -73,15 +128,57 @@ class RangeNet(BaseModule):
 
     def _make_stem_layer(self, in_channels: int, out_channels: int) -> None:  # tree conv blocks in beginning
         self.stem = nn.Sequential(
-            ConvModule(in_channels, out_channels, kernel_size=3, padding=1, conv_cfg=self.conv_cfg, norm_cfg=self.norm_cfg, act_cfg=self.act_cfg),
-            ConvModule(out_channels, out_channels, kernel_size=3, padding=1, conv_cfg=self.conv_cfg, norm_cfg=self.norm_cfg, act_cfg=self.act_cfg),
-            ConvModule(out_channels, out_channels, kernel_size=3, padding=1, conv_cfg=self.conv_cfg, norm_cfg=self.norm_cfg, act_cfg=self.act_cfg),
-        )  # conv block that bundles conv/norm/activation layers.
+            build_conv_layer(self.conv_cfg, in_channels, out_channels // 2, kernel_size=3, padding=1, bias=False),
+            build_norm_layer(self.norm_cfg, out_channels // 2)[1],
+            build_activation_layer(self.act_cfg),
+            build_conv_layer(self.conv_cfg, out_channels // 2, out_channels, kernel_size=3, padding=1, bias=False),
+            build_norm_layer(self.norm_cfg, out_channels)[1],
+            build_activation_layer(self.act_cfg),
+            build_conv_layer(self.conv_cfg, out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            build_norm_layer(self.norm_cfg, out_channels)[1],
+            build_activation_layer(self.act_cfg),
+        )
+
+    def make_res_layer(
+        self,
+        inplanes: int,
+        planes: int,
+        num_blocks: int,
+        stride: int,
+        dilation: int,
+        conv_cfg: OptConfigType = None,
+        norm_cfg: ConfigType = dict(type="BN"),
+        act_cfg: ConfigType = dict(type="LeakyReLU"),
+    ) -> nn.Sequential:
+        downsample = None
+        if stride != 1 or inplanes != planes:  # downsample to match the dimensions
+            downsample = nn.Sequential(
+                build_conv_layer(conv_cfg, inplanes, planes, kernel_size=1, stride=stride, bias=False), build_norm_layer(norm_cfg, planes)[1]
+            )
+
+        layers = []
+        layers.append(
+            BasicBlock(
+                inplanes=inplanes,
+                planes=planes,
+                stride=stride,
+                dilation=dilation,
+                downsample=downsample,
+                conv_cfg=conv_cfg,
+                norm_cfg=norm_cfg,
+                act_cfg=act_cfg,
+            )
+        )
+        inplanes = planes
+        for _ in range(1, num_blocks):
+            layers.append(
+                BasicBlock(inplanes=inplanes, planes=planes, stride=1, dilation=dilation, conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg)
+            )  # add the residual blocks
+        return nn.Sequential(*layers)
 
     def forward(self, x: Tensor) -> Tuple[Tensor]:
         x = self.stem(x)
         outs = [x]
-
         for layer_name in self.res_layers:
             res_layer = getattr(self, layer_name)
             x = res_layer(x)
