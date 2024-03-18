df7814588a486aa6d11c08fe2a714f49fe20fb82
liumu4090 <1356981019@qq.com>
2024-03-18 20:56:44 +0800

    www

--- projects/occ_paper/configs/ssc.py
+++ projects/occ_paper/configs/ssc.py
@@ -32,8 +32,8 @@ visualizer = dict(type=OccLocalVisualizer, vis_backends=vis_backends, name="visu
 
 custom_imports = dict(imports=["projects.occ_paper.mmdet3d_plugin"], allow_failed_imports=False)
 
-train_dataloader.update(dataset=dict(indices=5))
-train_dataloader.update(batch_size=2)
+# train_dataloader.update(dataset=dict(indices=5))
+# train_dataloader.update(batch_size=2)
 
-val_dataloader.update(batch_size=2)
+# val_dataloader.update(batch_size=2)
 # val_dataloader.update(dataset=dict(indices=5))
